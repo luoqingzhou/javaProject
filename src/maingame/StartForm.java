12dcf39df14d46464ad83fdab506d3a946164529
luoqinghzhou <1137486682@qq.com>
2021-05-08 02:12:48 +0800

    2021.5.8 morning push Game and StartForm just practice

---
+++ src/maingame/StartForm.java
@@ -0,0 +1,11 @@
+package maingame;
+
+import javax.swing.*;
+
+public class StartForm extends JFrame {
+    private JPanel mainPanel;
+    private JLabel lblBackground;
+    static public void main(String[] args) {
+        System.out.println("hello");
+    }
+}
