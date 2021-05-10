dd7c5abaf059e223d377d8e01d166dac882667e0
luoqinghzhou <1137486682@qq.com>
2021-05-11 01:10:52 +0800

    2021.5.11 凌晨 对于StartForm和Game的UI完善（未解决背景）

--- src/maingame/Game.java
+++ src/maingame/Game.java
@@ -1,4 +1,113 @@
 package maingame;
 
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Game extends JFrame{
+    private StartForm startform;
+    private static String path = System.getProperty("user.dir");
+
+    private int formWidth;
+    private int formHeight;
+
+    private boolean isPlaying = true;
+
+    private JLabel lblBackground;
+    private JButton btnRestartAndPause;
+    private JLabel lblScore;
+    private JTextField txtScore;
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JPanel gamePanel;
+
+    public Game(StartForm startform) {
+        super("marble");
+
+        this.startform = startform;
+        this.formWidth = startform.getWidth();
+        this.formHeight = startform.getHeight();
+
+        this.setUI();
+        this.setMiddle();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+
+    public void setMiddle() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int)screenSize.getWidth();
+        int screenHeight = (int)screenSize.getHeight();
+        this.setBounds((screenWidth - formWidth) / 2, (screenHeight - formHeight) / 2, formWidth, formHeight);
+    }
+
+    public void setUI() {
+//        ImageIcon img = new ImageIcon(path + "/resources/background1.png");
+//        lblBackground = new JLabel(img);
+//        lblBackground.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
+//        getLayeredPane().add(lblBackground, -30000);
+
+
+        mainPanel = (JPanel)this.getContentPane();
+        mainPanel.setOpaque(false);
+
+        mainPanel.setLayout(new BorderLayout());
+        topPanel = getTopPanel();
+        gamePanel = getGamePanel();
+        topPanel.setOpaque(false);
+        gamePanel.setOpaque(false);
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(gamePanel, BorderLayout.CENTER);
+    }
+
+    private JPanel getTopPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        btnRestartAndPause = new JButton("pause");
+        btnRestartAndPause.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        lblScore = new JLabel("current score");
+        lblScore.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        txtScore = new JTextField("000");
+        txtScore.setFont(new Font("Times New Roman", Font.BOLD, 20));
+
+        panel.add(btnRestartAndPause);
+        panel.add(lblScore);
+        panel.add(txtScore);
+
+        BtnListener btnListener = new BtnListener();
+        btnRestartAndPause.addActionListener(btnListener);
+
+        return panel;
+    }
+
+    private JPanel getGamePanel() {
+        JPanel panel = new JPanel();
+        System.out.println("gamePanel created");
+        return panel;
+    }
+
+    class BtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == btnRestartAndPause) {
+                if (isPlaying) {
+                    isPlaying = false;
+                    btnRestartAndPause.setText("restart");
+                    JOptionPane.showMessageDialog(Game.this, "pause");
+                }
+                else {
+                    isPlaying = true;
+                    btnRestartAndPause.setText("pause");
+                    JOptionPane.showMessageDialog(Game.this, "restart");
+                }
+            }
+        }
+    }
 }
--- src/maingame/StartForm.java
+++ src/maingame/StartForm.java
@@ -6,37 +6,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class StartForm extends JFrame {
 
     static public void main(String[] args) {
         new StartForm();
     }
-
+    private static String path = System.getProperty("user.dir");
     private final int formWidth = 450;
     private final int  formHeight = 700;
     private JPanel mainPanel;
-    private JPanel showPanel;
-    private JPanel gamePanel;
-    private JPanel menuPanel;
     private JLabel lblBackground;
     private JButton btnStart;
     private JButton btnExit;
-    private JButton btnPause;
-    private JButton btnRestart;
     private JButton btnRank;
-    private JButton btnMenu;
 
-
+    protected JLabel lbl;
 
     public StartForm() {
         super("marble");
+        //this.setBackground();
         this.setPanel();
         this.setBtn();
         this.setMiddle();
+
+
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setVisible(true);
+
     }
 
     public void setMiddle() {
@@ -49,36 +53,40 @@ public class StartForm extends JFrame {
     public void setBtn() {
         btnStart = new JButton("start");
         btnExit = new JButton("exit");
+        btnRank = new JButton("rank");
 
         btnStart.setFont(new Font("Times New Roman", Font.BOLD, 20));
         btnExit.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        btnRank.setFont(new Font("Times New Roman", Font.BOLD, 20));
 
         BtnListener btnListener = new BtnListener();
         btnStart.addActionListener(btnListener);
         btnExit.addActionListener(btnListener);
+        btnRank.addActionListener(btnListener);
 
-        showPanel.add(btnStart);
-        gamePanel.add(btnExit);
+        btnStart.setBounds(175, 300, 100, 50);
+        btnExit.setBounds(175, 400, 100, 50);
+        btnRank.setBounds(175, 500, 100, 50);
+
+
+        mainPanel.add(btnStart);
+        mainPanel.add(btnExit);
+        mainPanel.add(btnRank);
+
+
     }
 
     public void setPanel() {
         mainPanel = (JPanel)this.getContentPane();
-        showPanel = new JPanel();
-        gamePanel = new JPanel();
-        menuPanel = new JPanel();
+        mainPanel.setLayout(null);
+        mainPanel.setOpaque(false);
+    }
 
-        mainPanel.setSize(formWidth, formHeight);
-        showPanel.setBounds(0, 0, formWidth, (int)(0.1 * formHeight));
-        gamePanel.setBounds(0, (int)(0.1 * formHeight), formWidth, (int)(0.8 * formHeight));
-        menuPanel.setBounds(0, (int)(0.9 * formHeight), formWidth, (int)(0.1 * formHeight));
-
-        mainPanel.setLayout(new GridLayout(3, 1));
-        showPanel.setLayout(new FlowLayout());
-        menuPanel.setLayout(new FlowLayout());
-
-        mainPanel.add(showPanel);
-        mainPanel.add(gamePanel);
-        mainPanel.add(menuPanel);
+    public void setBackground() {
+        ImageIcon img = new ImageIcon(path + "/resources/background1.png");
+        lblBackground = new JLabel(img);
+        lblBackground.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
+        getLayeredPane().add(lblBackground, -30000);
     }
 
 
@@ -86,10 +94,29 @@ public class StartForm extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == btnStart) {
-                JOptionPane.showMessageDialog(mainPanel, "start");
+                System.out.println("ready to new Game");
+                new Game(StartForm.this);
+                StartForm.this.setVisible(false);
             }
             else if (e.getSource() == btnExit) {
-                JOptionPane.showMessageDialog(mainPanel, "exit");
+                System.exit(0);
+            }
+            else if (e.getSource() == btnRank) {
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(new File(path + "/rank/rank.txt")));
+                    String temp = reader.readLine();
+                    int i = 1;
+                    String message = "rank\n";
+                    while (temp != null && i <= 5) {
+                        message += String.valueOf(i) + ": " + temp + "\n";
+                        temp = reader.readLine();
+                        i++;
+                    }
+                    JOptionPane.showMessageDialog(StartForm.this, message);
+                    reader.close();
+                }catch (IOException exp) {
+                    System.out.println("rank error");
+                }
             }
             else {
                 System.out.println("error");
